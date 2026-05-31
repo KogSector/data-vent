ccdf23172d76868831ed5d2e539b59dc51d36f7d
Rishabh Das <rishabh.babi@gmail.com>
2026-06-01 01:39:04 +0530

    .

--- app/proto/retrieval_pb2_grpc.py
+++ app/proto/retrieval_pb2_grpc.py
@@ -3,9 +3,9 @@
 import grpc
 import warnings
 
-import retrieval_pb2 as retrieval__pb2
+from . import retrieval_pb2 as retrieval__pb2
 
-GRPC_GENERATED_VERSION = '1.78.0'
+GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
