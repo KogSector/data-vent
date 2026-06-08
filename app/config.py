fee61afcf2ed98b789b265cc12cb7fd91bfb6607
Rishabh Das <rishabh.babi@gmail.com>
2026-06-08 19:25:27 +0530

    .

--- app/config.py
+++ app/config.py
@@ -56,6 +56,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ("../.env", "../.env.secret")
         case_sensitive = True
+        extra = "ignore"
 
 
 # Global settings instance
